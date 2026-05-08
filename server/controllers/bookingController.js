2451d9019c76f515a22817ba1063b742405f47fe
CODEBOT <pankoustav@gmail.com>
2026-05-08 22:32:16 +0530

    fix: update bookingController to use capitalized User model

--- server/controllers/bookingController.js
+++ server/controllers/bookingController.js
@@ -1,5 +1,5 @@
 const Booking = require('../models/Booking');
-const User = require('../models/user');
+const User = require('../models/User');
 
 // @desc    Create new test drive booking
 // @route   POST /api/bookings
